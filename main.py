bec25e3775b3619d4f34ef34a0205003ff442286
DeVriesMatt <48200340+DeVriesMatt@users.noreply.github.com>
2020-07-23 15:34:33 +0100

    R2UIternet update

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import random
 
 def main(config):
     cudnn.benchmark = True
-    if config.model_type not in ['U_Net','R2U_Net','AttU_Net','R2AttU_Net', 'Iternet', 'AttUIternet', 'R2UItenet']:
+    if config.model_type not in ['U_Net','R2U_Net','AttU_Net','R2AttU_Net', 'Iternet', 'AttUIternet', 'R2UIternet']:
         print('ERROR!! model_type should be selected in U_Net/R2U_Net/AttU_Net/R2AttU_Net')
         print('Your input for model_type was %s'%config.model_type)
         return
