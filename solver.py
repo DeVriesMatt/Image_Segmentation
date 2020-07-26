69fa1b2ac0e75fee83fda39b1dcc65368b6fdf66
DeVriesMatt <48200340+DeVriesMatt@users.noreply.github.com>
2020-07-26 12:00:20 +0100

    Update solver

--- solver.py
+++ solver.py
@@ -35,7 +35,7 @@ class Solver(object):
 		self.optimizer = None
 		self.img_ch = config.img_ch
 		self.output_ch = config.output_ch
-		self.criterion = torch.nn.BCELoss()  # TODO: Look at changing
+		self.criterion = torch.nn.BCEWithLogitsLoss() #       BCELoss()  # TODO: Look at changing
 		self.augmentation_prob = config.augmentation_prob
 		# self.image_size = config.
 
