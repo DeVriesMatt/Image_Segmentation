06e7a20029ae8936fb5dc3f490be81f6447e6efe
DeVriesMatt <48200340+DeVriesMatt@users.noreply.github.com>
2020-07-26 11:56:38 +0100

    Update solver

--- solver.py
+++ solver.py
@@ -168,7 +168,7 @@ class Solver(object):
 		#
 		# 	return loss
 		#
-		trial = Trial(self.unet, self.optimizer, self.criterion, metrics=['loss', mean(get_DC)]).to(self.device)
+		trial = Trial(self.unet, self.optimizer, self.criterion, metrics=['loss', 'acc']).to(self.device)
 		trial.with_generators(train_generator=self.train_loader,
 							  val_generator=self.valid_loader,
 							  test_generator=self.test_loader)
