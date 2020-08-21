f9733f245db78d52d62a4abb8ec1ddd296cf3c66
DeVriesMatt <48200340+DeVriesMatt@users.noreply.github.com>
2020-08-21 15:27:53 +0100

    image channel

--- solver.py
+++ solver.py
@@ -92,7 +92,7 @@ class Solver(object):
 		elif self.model_type =='R2U_Net':
 			self.unet = R2U_Net(img_ch=3,output_ch=1, t=self.t)  # TODO: changed for green image channel
 		elif self.model_type =='AttU_Net':
-			self.unet = AttU_Net(img_ch=3,output_ch=1)
+			self.unet = AttU_Net(img_ch=1,output_ch=1)
 		elif self.model_type == 'R2AttU_Net':
 			self.unet = R2AttU_Net(img_ch=3,output_ch=1, t=self.t)
 		elif self.model_type == 'Iternet':
